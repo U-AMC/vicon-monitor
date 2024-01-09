213b33e86d8538b1b725520f86663214cd21d223
Jason Kim <43529281+U-AMC@users.noreply.github.com>
2024-01-09 11:42:03 +0900

    Update vicon_tf.py
    vicon monitor now directly monitors vicon_bridge tf tree.
    Custom frame (e.g "map" ) is now published directly for custom connectivity with sensor base_link

--- scripts/vicon_tf.py
+++ scripts/vicon_tf.py
@@ -8,7 +8,6 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped, TransformStamped, Pose, Point, Quaternion
 from tf2_msgs.msg import TFMessage
 import tf, tf2_ros
-import tf.transformations
 #system
 import copy
 import _thread
@@ -19,53 +18,75 @@ cur_odom_to_baselink = None
 cur_map_to_odom = None
 
 class VT:
-    def __init__(self, args):
-        ## about ROS
-        rospy.init_node("vicon_ros_monitor")
+    def __init__(self):
+
         ##odom listener
         # self.sub_odom = rospy.Subscriber(vins_topic, Odometry, self.odometryCb)
 
-        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(1.0))
-        self.transformer =  tf.TransformListener(self.tf_buffer)
+        self.tf_buffer = tf2_ros.Buffer()
+        self.transformer = tf2_ros.TransformListener(self.tf_buffer)
 
-        ## custom tf
-        # self.rgb_to_parallel_tf_broadcaster = tf.TransformBroadcaster()
+        # arguments
+        self.base_frame = rospy.get_param('~base_frame')
+        self.pose_frame = rospy.get_param('~pose_frame')
+        self.sudo_frame = rospy.get_param('~sudo_frame')
+        self.map_frame = rospy.get_param('~map_frame')
+        self.pose_pub = rospy.Publisher('~pose', PoseStamped, queue_size = 1)
+        self.custom_tf = tf.TransformBroadcaster()
+        rospy.loginfo('base frame as...: ' + self.base_frame)
+        rospy.loginfo('pose frame as...: ' + self.pose_frame)
+    
+    def handle_tf(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            try:
+                # self.tf_buffer.waitForTransform(self.base_frame,self.pose_frame, rospy.Time.now(), rospy.Duration(0.1))
+                transform = self.tf_buffer.lookup_transform(self.base_frame,self.pose_frame,rospy.Time.now(), rospy.Duration(1.0)) #rot is quaternion, trans is xyz vector ([x, y, z], [x, y, z, w])
+                #pose
+                x_t = transform.transform.translation.x
+                y_t = transform.transform.translation.y
+                z_t = transform.transform.translation.z
+                #rot
+                x_r = transform.transform.rotation.x
+                y_r = transform.transform.rotation.y  
+                z_r = transform.transform.rotation.z
+                w_r = transform.transform.rotation.w 
+                
+            except(tf2_ros.LookupException,
+                   tf2_ros.ConnectivityException,
+                   tf2_ros.ExtrapolationException,
+                   tf2_ros.TransformException) as e:
+                print("Exception occurred : ", e)
+                continue
 
-    def odometryCb(self, msg1):
-        # print (msg1.pose.pose)
-        self.agent_odom = msg1
+            self.custom_tf.sendTransform(translation=[x_t,y_t,z_t],rotation=[x_r,y_r,z_r,w_r],time=rospy.Time.now(),child='map',parent=self.base_frame)
+            #publish pose
+            pose = PoseStamped()
+            pose.header.frame_id = self.base_frame
+            pose.header.stamp = transform.header.stamp
+            pose.pose.position.x = transform.transform.translation.x
+            pose.pose.position.y = transform.transform.translation.y
+            pose.pose.position.z = transform.transform.translation.z
+            pose.pose.orientation = transform.transform.rotation
+            self.pose_pub.publish(pose)
+            
+            rate.sleep()
+
     
     def pose_to_mat(pose_msg):
         return np.matmul(
         tf.listener.xyz_to_mat44(pose_msg.pose.pose.position),
         tf.listener.xyzw_to_mat44(pose_msg.pose.pose.orientation),
         )
-    
-    def handle_tf(self, msg1):
-        global cur_odom_to_baselink, cur_vicon_to_odom
-        msg_time = msg1.header.stamp
-        try:
-            self.transformer.waitForTransform(pose_frame, base_frame, msg_time, rospy.Duration(0.1))
-            trans, rot = self.transformer.lookupTransform(pose_frame, base_frame, msg_time) #rot is quaternion, trans is xyz vector ([x, y, z], [x, y, z, w])
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException, tf2_ros.TransformException) as e: #exception case
-            print("Exception occurred : ", e)
-            return
-
-        br = tf.TransformBroadcaster()
-        br.sendTransform(trans,rot, rospy.Time.now(), "map","sudo")
-
 
 
 if __name__ == '__main__':
-    # arguments
-    base_frame = rospy.get_param('~base_frame')
-    pose_frame = rospy.get_param('~pose_frame')
-    sudo_frame = rospy.get_param('~sudo_frame')
-    map_frame = rospy.get_param('~map_frame')
-        
+    ## about ROS
+    rospy.init_node("vicon_ros_monitor")
     ## node open
     vicon_node = VT()
     try:
+        vicon_node.handle_tf()
         rospy.spin()
     except KeyboardInterrupt:
         print("Shut Down")
