744e32a1c8c670bbad9d67f013c1b099b4b2c959
Jason Kim <43529281+U-AMC@users.noreply.github.com>
2024-01-09 12:07:53 +0900

    Update vicon_tf.py

--- scripts/vicon_tf.py
+++ scripts/vicon_tf.py
@@ -4,7 +4,7 @@ import rospy
 import math
 import numpy as np
 from sensor_msgs.msg import Imu
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, Path
 from geometry_msgs.msg import PoseStamped, TransformStamped, Pose, Point, Quaternion
 from tf2_msgs.msg import TFMessage
 import tf, tf2_ros
@@ -19,28 +19,40 @@ cur_map_to_odom = None
 
 class VT:
     def __init__(self):
-
+        self.x_t = None
+        self.y_t  = None
+        self.z_t = None
+        self.x_r = None
+        self.y_r = None
+        self.z_r = None
+        self.w_r = None
         ##odom listener
         # self.sub_odom = rospy.Subscriber(vins_topic, Odometry, self.odometryCb)
 
         self.tf_buffer = tf2_ros.Buffer()
-        self.transformer = tf2_ros.TransformListener(self.tf_buffer)
+        self.transformer = tf.TransformListener(self.tf_buffer)
 
         # arguments
         self.base_frame = rospy.get_param('~base_frame')
         self.pose_frame = rospy.get_param('~pose_frame')
         self.sudo_frame = rospy.get_param('~sudo_frame')
         self.map_frame = rospy.get_param('~map_frame')
+
+        #ros publishers
         self.pose_pub = rospy.Publisher('~pose', PoseStamped, queue_size = 1)
+        self.odom_pub = rospy.Publisher('~Odometry', PoseStamped, queue_size = 1)
+        self.vicon_path_pub = rospy.Publisher('~vicon_path', Path, queue_size=1)
         self.custom_tf = tf.TransformBroadcaster()
-        rospy.loginfo('base frame as...: ' + self.base_frame)
-        rospy.loginfo('pose frame as...: ' + self.pose_frame)
+        self.vicon_path = Path()
+        #log info for launch param
+        rospy.loginfo('Base_frame as...: ' + self.base_frame)
+        rospy.loginfo('Pose_frame as...: ' + self.pose_frame)
     
     def handle_tf(self):
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             try:
-                # self.tf_buffer.waitForTransform(self.base_frame,self.pose_frame, rospy.Time.now(), rospy.Duration(0.1))
+                self.transformer.waitForTransform(self.base_frame,self.pose_frame, rospy.Time.now(), rospy.Duration(0.1))
                 transform = self.tf_buffer.lookup_transform(self.base_frame,self.pose_frame,rospy.Time.now(), rospy.Duration(1.0)) #rot is quaternion, trans is xyz vector ([x, y, z], [x, y, z, w])
                 #pose
                 x_t = transform.transform.translation.x
@@ -58,8 +70,14 @@ class VT:
                    tf2_ros.TransformException) as e:
                 print("Exception occurred : ", e)
                 continue
-
-            self.custom_tf.sendTransform(translation=[x_t,y_t,z_t],rotation=[x_r,y_r,z_r,w_r],time=rospy.Time.now(),child='map',parent=self.base_frame)
+            
+            #frame publisher
+            self.custom_tf.sendTransform(
+                translation=[x_t,y_t,z_t],
+                rotation=[x_r,y_r,z_r,w_r],
+                time=rospy.Time.now(),
+                child=self.map_frame, #define it as frame that you want to hold as world frame
+                parent=self.base_frame)
             #publish pose
             pose = PoseStamped()
             pose.header.frame_id = self.base_frame
@@ -70,23 +88,29 @@ class VT:
             pose.pose.orientation = transform.transform.rotation
             self.pose_pub.publish(pose)
             
+            #vicon path
+            self.vicon_path.header.frame_id=self.base_frame
+            self.vicon_path.poses.append(pose)
+            self.vicon_path.header.stamp = rospy.Time.now()
+            self.vicon_path_pub.publish(self.vicon_path)
+            
+            #spin rate
             rate.sleep()
-
     
+    #SE(3) conversion module 
     def pose_to_mat(pose_msg):
         return np.matmul(
         tf.listener.xyz_to_mat44(pose_msg.pose.pose.position),
-        tf.listener.xyzw_to_mat44(pose_msg.pose.pose.orientation),
-        )
-
-
+        tf.listener.xyzw_to_mat44(pose_msg.pose.pose.orientation))
+    
 if __name__ == '__main__':
     ## about ROS
     rospy.init_node("vicon_ros_monitor")
     ## node open
     vicon_node = VT()
     try:
-        vicon_node.handle_tf()
+        #run thread
+        _thread.start_new_thread(vicon_node.handle_tf, ())
         rospy.spin()
     except KeyboardInterrupt:
         print("Shut Down")
